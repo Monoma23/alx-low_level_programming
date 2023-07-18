39c488c5291d9221b2a09447cb43aa8218bb1a17
Ayoub <wydadi2352@gmail.com>
2023-07-18 12:27:10 -0700

    10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ char alphabets[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char alpha;
 int j;
-for (j = 0; j < 9; j++)
+for (j = 0; j <= 9; j++)
 {
 int i;
 for (i = 0; i < 26; i++)
