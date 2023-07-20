6add057c5571f53d2188ec1ad093295753817bc0
Ayoub <wydadi2352@gmail.com>
2023-07-20 05:46:28 -0700

    kool

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
 
 void print_most_numbers(void)
 {
-	char k;
+        char k;
 
-	for (k = '0'; k < '10'; k++)
-	{
-		if ((k != '2') || (k != '4'))
-			_putchar(k);
-	}
-	_putchar('\n');
+        for (k = '0'; k <= '9'; k++)
+        {
+                if ((k != '2') && (k != '4'))
+                        _putchar(k);
+        }
+        _putchar('\n');
 }
 
