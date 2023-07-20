f74da17a6bc597eef3e90dcc32e0840ce5a6ac3c
Ayoub <wydadi2352@gmail.com>
2023-07-20 05:48:53 -0700

    wow

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers -display bumbers sauf 2&4
- *
- * return: void
- */
+* print_most_numbers -display bumbers sauf 2&4
+*
+* return: void
+*/
 
 void print_most_numbers(void)
 {
-        char k;
+char k;
 
-        for (k = '0'; k <= '9'; k++)
-        {
-                if ((k != '2') && (k != '4'))
-                        _putchar(k);
-        }
-        _putchar('\n');
+for (k = '0'; k <= '9'; k++)
+{
+if ((k != '2') && (k != '4'))
+_putchar(k);
+}
+_putchar('\n');
 }
 
