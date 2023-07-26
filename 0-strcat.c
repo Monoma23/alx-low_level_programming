d34ad7a0afd39a6de0c5e8b925105099cfa78a77
Ayoub <wydadi2352@gmail.com>
2023-07-26 11:17:40 -0700

    teste

---
+++ 0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - combine 2 strings
+ * @dest: input 1
+ * @src: input 2
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src) {
+char *longofdest = dest;
+    
+while (*longofdest != '\0')
+{
+longofdest++;
+}
+    
+   
+while (*src != '\0')
+{
+*longofdest = *src;
+longofdest++;
+src++;
+}
+    
+*longofdest = '\0';
+return dest;
+}
