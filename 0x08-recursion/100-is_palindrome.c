accca3aff659bef0adf2d1a4aa8db136b22fc2d6
Ayoub <wydadi2352@gmail.com>
2023-08-03 10:39:06 -0700

    jojoj

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,29 @@
 #include "main.h"
+#include <stdbool.h>
 #include <string.h>
 
+/**
+ * is_palindrome_recursive - Recursive helper function
+ * to check if the string is a palindrome.
+ * @s: The string to be checked.
+ * @gauche: The gauche index of the current comparison.
+ * @right: The right index of the current comparison.
+ * Return: true if s is a palindrome, false otherwise.
+ */
+bool is_palindrome_recursive(char *s, int gauche, int right)
+{
+if (gauche >= right)
+{
+return (true);
+}
+
+if (s[gauche] != s[right])
+{
+return (false);
+}
+
+return (is_palindrome_recursive(s, gauche + 1, right - 1));
+}
 
 /**
  * is_palindrome - Checks if the input string is a palindrome.
@@ -10,19 +33,8 @@
 int is_palindrome(char *s)
 {
 int gauche = 0;
-int droit = strlen(s) - 1;
+int right = strlen(s) - 1;
 
-while (gauche < droit)
-{
-if (s[gauche] != s[droit])
-{
-return (0);
+return (is_palindrome_recursive(s, gauche, right));
 }
-gauche++;
-droit--;
-}
-
-return (1);
-}
-
 
