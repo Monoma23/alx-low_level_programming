5d3749466621e9a9d9871b81ea164260893ce439
Ayoub <wydadi2352@gmail.com>
2023-10-16 09:23:28 -0700

    task zero

---
+++ 0x18-dynamic_libraries/empty_test.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - just for testing purposes
+ *
+ * Return:EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("testo thisooo"));
+	return (EXIT_SUCCESS);
+}
